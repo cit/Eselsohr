cf0c98f5119ee4fc4d2e882c4c775ba4c7b9f3b0
Florian Adamsky <florian@adamsky.it>
2015-04-05 21:35:18 +0200

    Added new routes to Eselsohr.pm

--- lib/Eselsohr.pm
+++ lib/Eselsohr.pm
@@ -5,16 +5,34 @@ use Mojolicious::Plugin::Bcrypt;
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    ## Documentation browser under "/perldoc"
+    $self->plugin('PODRenderer');
+    $self->plugin('bcrypt', { cost => 4 });
 
-  # Router
-  my $r = $self->routes;
+    ## session secret to encrypt the session cookies
+    $self->app->secrets(['73ba117e0de616d437080866177c3f90']);
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    ## Router
+    my $r = $self->routes;
+
+    $r->any('/login')->to('auth#login');
+    $r->get('/logout')->to('auth#logout');
+
+    ## Redirect to login if there is no valid username in the session
+    ## variable
+    my $auth = $r->under( sub {
+        my $self = shift;
+
+        unless ($self->session('username')) {
+            $self->redirect_to('/login');
+        }
+    });
+
+    ## Normal route to controller
+    $auth->get('/')->to('example#welcome');
+    $auth->get('/:username/')->to('bookmarks#show_all');
 }
 
 1;

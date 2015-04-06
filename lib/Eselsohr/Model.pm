f1791f1de5974fcafc708d3b3891be0e9a7a38c4
Florian Adamsky <florian@adamsky.it>
2015-04-06 21:37:42 +0200

    Removed no longer required code

--- lib/Eselsohr/Model.pm
+++ lib/Eselsohr/Model.pm
@@ -32,24 +32,4 @@ use ORLite {
 
 };
 
-package Eselsohr::Model::Bookmarks;
-
-sub count_all {
-    my ($self, $id) = @_;
-
-    Eselsohr::Model::Bookmarks->count('WHERE user_id=?', $id);
-}
-
-sub select_all {
-    my ($self, @args_hash) = @_;
-    my $args = { @args_hash };
-
-    my $query  = 'WHERE user_id=? ORDER BY id DESC LIMIT ? OFFSET ?';
-    my $offset = (($args->{page} - 1) * $args->{limit});
-
-    Eselsohr::Model::Bookmarks->select($query, $args->{user_id},
-        $args->{limit}, $offset);
-}
-
-
 1;

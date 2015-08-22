66828928c8e2148051a1779621c4746beff3976b
Claudia <claudi-git@haktar.org>
2015-08-22 19:54:44 +0200

    add new navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,44 @@
-/**
-* click event to slide down loggin form
-*
-*/
 
-$('#loginBtn').click(function(event){
-	$(this).slideUp('200');
-	$("form[name='login']").slideDown('230');
-	
-});	
+
+/* Popover for search and add new link*/
+var initPopover = function(elm){
+	var $elmId = $('#' + elm);
+	var $elmClass = $('.' + elm);
+
+	$elmId.popover({
+	    html: true,
+	    placement: 'bottom',
+	    title: function () {
+	        return $elmClass.find('.head').html();
+	    },
+	    content: function () {
+	        return $elmClass.find('.content').html();
+	    }
+	}).click(function(event){
+		/*Clear input field of search popover*/
+		$(".js-hasclear").on('keyup', function () {
+		    var t = $(this);
+		    t.next('span').toggle(Boolean(t.val()));
+		});
+		$(".js-clearer").hide($(this).prev('input').val());
+		console.log($(".js-clearer"));
+		$(".js-clearer").on('click',function () {
+			console.log('foobar!');
+		    $(this).prev('input').val('').focus();
+		    $(this).hide();
+		});
+	});
+};
+
+initPopover('searchPopover');
+initPopover('addPopover');
+
+$('.js-openPopover').on('click', function(event){
+	$('.js-openPopover').not(this).popover('hide');
+});
+/*****************************************/
+
+
 
 
 var username = $('table').attr('data-user');
